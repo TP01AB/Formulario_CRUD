8bc631be3538dcdf881b4944e1ca0a5140e426df
israel <isra9movil@hotmail.com>
2020-09-30 11:10:10 +0200

    Bienvenido
    Pagina de bienvenida para el usuario logueado correctamente

--- src/java/datos/Usuario.java
+++ src/java/datos/Usuario.java
@@ -10,8 +10,9 @@ package datos;
  * @author isra9
  */
 public class Usuario {
-    
+
     String email;
+    String nombre;
     String pass;
     int rango;
 
@@ -19,9 +20,38 @@ public class Usuario {
     }
 
     public Usuario(String email, String pass) {
+        this.nombre = nombreAleat();
         this.email = email;
         this.pass = pass;
         this.rango = 0;
+    }
+
+    private String nombreAleat() {
+        int n = (int) (Math.random() * 4);
+        String nombre = null;
+        switch (n) {
+            case 0:
+                nombre = "Israel";
+                break;
+            case 1:
+                nombre = "Jonh";
+                break;
+            case 2:
+                nombre = "Belen";
+                break;
+            case 3:
+                nombre = "Alvaro";
+                break;
+        }
+        return nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getEmail() {
@@ -50,7 +80,7 @@ public class Usuario {
 
     @Override
     public String toString() {
-        return "Usuario{" + "email=" + email + ", pass=" + pass + ", rango=" + rango + '}';
+        return "Usuario{" + "email=" + email + ", nombre=" + nombre + ", rango=" + rango + '}';
     }
 
 }
